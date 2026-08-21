5c29015df6c05b9604cc76911002128b505b2409
trandang2507 <trandang25207@gmail.com>
2026-08-21 21:54:39 +0700

    thêm scroll hiện trên thanh nav

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ function showPage( id ){
         behavior: "smooth"
     });
 }
+
+const sections = document.querySelectorAll(".page-sections");
+const navButtons = document.querySelectorAll(".nav-link button");
+
+const observe = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            navButtons.forEach(button => {
+                button.classList.remove("button-active");
+            });
+
+            const activeButton = document.querySelector(
+                `button[onclick="showPage('${entry.target.id}')"]`
+            );
+
+            if (activeButton) {
+                activeButton.classList.add("button-active");
+            }
+        }
+    });
+},{
+    threshold: 0.5
+}
+);
+
+sections.forEach(section => observe.observe(section));
